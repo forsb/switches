165583c6758b012c653023a807f099f8871d0670
Filip Forsby <filip@forsby.nu>
2016-12-17 13:09:41 +0100

    working output from arduino

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -11,6 +11,8 @@
    8 - UNUSED  
 */
 
+#include <stdint.h>
+
 //nRF24L01
 #include <SPI.h>
 #include "RF24.h"
@@ -35,7 +37,7 @@
 const uint64_t pipe = 0xADADADADE1LL;
 //char message[] = "buttonMox";
 //SensorPayload message;
-char message[9];
+uint8_t message[9];
 
 uint16_t packageCount = 0;
 uint16_t blinkCount = 0;
@@ -58,56 +60,110 @@ void setup() {
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(76);
-  radio.setCRCLength(RF24_CRC_8); 
-  radio.printDetails(); 
+  radio.setCRCLength(RF24_CRC_8);
+  radio.openWritingPipe(pipe);
 
   // Set sensor ID
-  message[0] = 1;
+  message[0] = 12;
 }
 
 void loop() {  
   //enterSleep();
   
   // --- DHT22 ---
-  Serial.print("DHT22, \t");
+  //Serial.print("DHT22, \t");
   int chk = DHT.read22(DHT_PIN);
   
   if (chk != DHTLIB_OK) {
-    Serial.print("DHT error."); 
-    Serial.println(chk);
+    //Serial.print("DHT error."); 
+    //Serial.println(chk);
   }
   else {
-    Serial.print(DHT.humidity, 1);
-    Serial.print(",\t");
-    Serial.println(DHT.temperature, 1);
+    //Serial.print(DHT.humidity, 1);
+    //Serial.print(",\t");
+    //Serial.println(DHT.temperature, 1);
   }
 
   // --- PHOTO ---
   int photoval = analogRead(PHOTO_PIN);
   int potval = analogRead(POT_PIN);
-  Serial.print("PHOTO, \t");
-  Serial.println(photoval-potval);
+  //Serial.print("PHOTO, \t");
+  //Serial.println(photoval-potval);
   if (photoval-potval > 0) {
     blinkCount ++;
   }
   
   
-  
-  int16_t temp = (int) DHT.temperature*100;
-  int16_t humid = (int) DHT.humidity*100;
-  message[1] = (packageCount | 0xF0) >> 8;
-  message[2] = (packageCount | 0x0F);
-  message[3] = (temp | 0xF0) >> 8;
-  message[4] = (temp | 0x0F);
-  message[5] = (humid | 0xF0) >> 8;
-  message[6] = (humid | 0x0F);
-  message[7] = (blinkCount | 0xF0) >> 8;
-  message[8] = (blinkCount | 0x0F);
-  
-  // --- RADIO ---
 
   
-  packageCount ++;
+  
+  
+  
+/*  message[1] = (packageCount >> 8) & 0x0F;
+  message[2] = packageCount & 0x0F;
+  message[3] = (temp >> 8) & 0x0F;
+  message[4] = temp & 0x0F;
+  message[5] = (humid >> 8) & 0xF;
+  message[6] = humid & 0x0F;
+  message[7] = (blinkCount >> 8) & 0xF;
+  message[8] = blinkCount & 0x0F;*/
+  
+  /*Serial.print("Recv: payload number ");
+  Serial.print(packageCount);
+  Serial.print(" from ");
+  Serial.println(message[0]);
+  Serial.print("temp: ");
+  Serial.print(temp);
+  Serial.print(" humid: ");
+  Serial.print(humid);
+  Serial.print(" blink: ");
+  Serial.println(blinkCount);*/
+  
+  int16_t temp = (int) (DHT.temperature*100);
+  int16_t humid = (int) (DHT.humidity*100);
+  
+  uint16_t* mcount = (uint16_t*) &message[1];
+  int16_t* mtemp = (int16_t*) &message[3];
+  int16_t* mhumid = (int16_t*) &message[5];
+  uint16_t* mblink = (uint16_t*) &message[7];
+  *mcount = packageCount;
+  *mtemp = temp;
+  *mhumid = humid;
+  *mblink = blinkCount;
+  
+  // --- RADIO ---
+  radio.write( message, sizeof(message));
+  
+  /*
+    0    1    2    3    4    5    6    7    8     
+  |----|----.----|----.----|----.----|----.----|
+    ID    count     temp      humid     blink
+  */
+  uint8_t* sensorId2 = &message[0];
+  uint16_t* packageCount2 = (uint16_t*) &message[1];
+  int16_t* temp2 = (int16_t*) &message[3];
+  int16_t* humid2 = (int16_t*) &message[5];
+  uint16_t* blinkCount2 = (uint16_t*) &message[7];
+  /*
+  uint8_t sensorId2 = message[0];
+  int16_t packageCount2 = (message[1] << 8) | message[2];
+  int16_t temp2 = (message[3] << 8) | message[4];
+  int16_t humid2 = (message[5] << 8) | message[6];
+  int16_t blinkCount2 = (message[7] << 8) | message[8];
+  */
+    /*
+  Serial.print("Recv: payload number ");
+  Serial.print(*packageCount2);
+  Serial.print(" from ");
+  Serial.println(*sensorId2);
+  Serial.print("temp: ");
+  Serial.print(*temp2);
+  Serial.print(" humid: ");
+  Serial.print(*humid2);
+  Serial.print(" blink: ");
+  Serial.println(*blinkCount2);*/
+  
+  packageCount += 1;
   
   delay(1000);
   //Serial.println(message);
